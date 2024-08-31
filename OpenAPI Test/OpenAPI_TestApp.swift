1947dc70dca23f80d5b47e603dfb5482d9ca2497
Manjit Bedi <117043901+ManjitBediBC@users.noreply.github.com>
2024-08-31 09:48:22 -0700

    Initial Commit

---
+++ OpenAPI Test/OpenAPI_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenAPI_TestApp.swift
+//  OpenAPI Test
+//
+//  Created by Manjit Bedi on 2024-08-31.
+//
+
+import SwiftUI
+
+@main
+struct OpenAPI_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
